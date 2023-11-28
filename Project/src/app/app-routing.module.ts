8c0797885d547011d41b9c3b2fd13561a22a347d
Mohsin-Git <mohsinwaqas.bscs@gmail.com>
2023-11-28 23:42:34 +0500

    Create admin changes

--- Project/src/app/app-routing.module.ts
+++ Project/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ForgotPasswordComponent } from './LoginRelated_Pages/forgot-password/forgot-password.component';
+import { LoginComponent } from './LoginRelated_Pages/login/login.component';
+import { NotFoundComponent } from './LoginRelated_Pages/not-found/not-found.component';
+// import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+// import { LoginComponent } from './components/login/login.component';
+ 
+const routes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'forgot-password', component: ForgotPasswordComponent},
+  {path:'', redirectTo:'/login',pathMatch:'full'},
+  {
+   path:'admin', 
+   loadChildren:() => import('./modules/admin/admin.module').then((m)=> m.AdminModule),
+  },
+  {path:'**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ForgotPasswordComponent } from './LoginRelated_Pages/forgot-password/forgot-password.component';
+import { LoginComponent } from './LoginRelated_Pages/login/login.component';
+import { NotFoundComponent } from './LoginRelated_Pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
